346bea86ccaa3491b3b28674dfacb8bcbc2db537
Christopher Armstrong <chstrong@gmail.com>
2022-12-31 09:12:24 +0800

    User Signup Test working

--- test/e2e/confirm-user-signup.test.ts
+++ test/e2e/confirm-user-signup.test.ts
@@ -2,16 +2,16 @@ const given = require('../lib/steps/given')
 const when = require('../lib/steps/when')
 const then = require('../lib/steps/then')
 
-describe('When confirmUserSignup runs', () => {
+describe('When a user signs up', () => {
     it('The user\'s profile should be saved in DynamoDB', async () => {
-        const { name, email } = given.a_random_user()
+        const { name, email, password } = given.a_random_user()
         
-        await when.we_invoke_confirmUserSignup(email, name)      
+        const user = await when.a_user_signs_up(name, email, password)      
 
-        const ddbUser = await then.user_exists_in_UserTable(email)
+        const ddbUser = await then.user_exists_in_UserTable(user.email)
         expect(ddbUser).toMatchObject({
-            id: email,
-            name: name,
+            id: user.email,
+            name: user.name,
             createdAt: expect.stringMatching(/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d(?:\.\d+)?Z?/g),
             followersCount: 0,
             followingCount: 0,
--- test/lib/steps/when.ts
+++ test/lib/steps/when.ts
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const AWS = require('aws-sdk')
 
 const we_invoke_confirmUserSignup = async (email: String, name: String) => {
     const handler = require('../../../lib/lambda/cognito/confirm-user-signup.ts').handler
@@ -26,6 +27,41 @@ const we_invoke_confirmUserSignup = async (email: String, name: String) => {
     await handler(event, context)
 }
 
-module.exports = {
-    we_invoke_confirmUserSignup
+const a_user_signs_up = async (name: String, email: String, password: String) => {
+    const cognito = new AWS.CognitoIdentityServiceProvider()
+
+    const userPoolId = process.env.COGNITO_USER_POOL_ID
+    // const clientId = ...
+
+    const signUpResp = await cognito.signUp({
+        ClientId: process.env.COGNITO_USER_POOL_CLIENT_ID,
+        Username: email,
+        Password: password,
+        UserAttributes: [
+            { Name: 'name', Value: name }
+        ]
+    }).promise()
+
+    const username = signUpResp.UserSub
+    console.log(`[${email}] - user has signed up [${name}]`)
+
+    await cognito.adminConfirmSignUp({
+        UserPoolId: userPoolId,
+        Username: username,
+    }).promise()
+
+    console.log(`${email} - confirmed sign up`)
+
+    return {
+        username,
+        name,
+        email
+    }
 }
+
+module.exports = {
+    we_invoke_confirmUserSignup,
+    a_user_signs_up
+}
+
+export {}
